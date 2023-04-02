f1b71733a14eab8514df7f8eb312b4973450816e
Olivernio <diegoozzysh@gmail.com>
2023-04-02 01:05:57 -0400

    Commit 12
    Un par de errores en la consola arreglados.

--- js/index/script.js
+++ js/index/script.js
@@ -11,8 +11,7 @@ $(function () {
       if (email.length >= 3 && nombre.length >= 3 && tel.length === 9 && mensaje.length >= 3){
   
         alert("Muchas gracias "+nombre+", pronto le enviaremos un correo electrónico.")
-  
-      $('#container>#container-comentarios').append(fila);
+
       }else{
       
         alert("Verifique los requerimientos.")
